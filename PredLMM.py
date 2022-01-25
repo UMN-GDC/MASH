68be7e3245b824155477123e265490b4c40a9a78
Christian Coffman <coffm049@ln0006.mesabi.msi.umn.edu>
2022-01-25 09:03:38 -0600

    moved argument parser for PredLMM to it's own function

--- PredLMM.py
+++ PredLMM.py
@@ -14,39 +14,14 @@ from scipy.optimize import newton
 from scipy.linalg.blas import dgemm,sgemm,sgemv
 from copy import copy
 import pandas as pd 
-
-
-#%%
-#-----------------------Set up user inputs for parsing----------------------------------------
-start_time = timeit.default_timer()
-parser = argparse.ArgumentParser(prog='Running PredLMM',description='In a general scenario, one should compute the GRM files first with the PLINK Binary files using GCTA software. Then, follow the jupyter notebook: PredLMM_notebook for estimating heribaility and variance of a phenotype adjusting the availble covariates.',formatter_class=RawTextHelpFormatter)
-parser.add_argument('--prefix', type=str, help='prefix for GCTA format GRM files, including PREFIX.grm.bin, PREFIX.grm.N.bin, and PREFIX.grm.id [required]',required=True)
-parser.add_argument('--covar', type=str, help='Read PLINK format covariate file contains covariates besides PCs to be adjusted')
-parser.set_defaults(covar="NULL")
-parser.add_argument('--pheno', type=str, help='Read PLINK format phenotype file [required]\nIf --mpheno is not specified then then 3rd column (the 1st phenotype) will be used.', required=True)
-parser.add_argument('--mpheno',type=int, default=1,help='Specify which phenotype to use from phenotype file (1 phenotype only)')
-parser.set_defaults(mpheno=1)
-parser.add_argument('--out',type=str, help='Specify the output file name. [required]',required=True)
-parser.add_argument('--std',action='store_true',default=False,help='Run SAdj-HE (i.e., with standardization)')
-
-args = parser.parse_args()
-#%%%
-prefix= "Example/grm"
-covar = "Example/covar.csv"
-pheno = "Example/pheno.phen"
-outprefix = "lkajfkjads"
-
-#%%
-outprefix = args.out
+from functions.PredLMM_parser import *
+from functions.PredLMM_parser import *
 
 
 # logging.basicConfig(format='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s',
 #                     level=logging.DEBUG,filename=outprefix+'.log',filemode='a')
 # for arg, value in sorted(vars(args).items()):
 #     logging.info("Argument %s: %r", arg, value)
-
-prefix = args.prefix
-
 
 start_read = timeit.default_timer()
 #%%
---
+++ functions/PredLMM_parser.py
@@ -0,0 +1,28 @@
+import timeit
+import logging
+import resource
+import argparse
+from argparse import RawTextHelpFormatter
+
+#-----------------------Set up user inputs for parsing----------------------------------------
+start_time = timeit.default_timer()
+parser = argparse.ArgumentParser(prog='Running PredLMM',description='In a general scenario, one should compute the GRM files first with the PLINK Binary files using GCTA software. Then, follow the jupyter notebook: PredLMM_notebook for estimating heribaility and variance of a phenotype adjusting the availble covariates.',formatter_class=RawTextHelpFormatter)
+parser.add_argument('--prefix', type=str, help='prefix for GCTA format GRM files, including PREFIX.grm.bin, PREFIX.grm.N.bin, and PREFIX.grm.id [required]',required=True)
+parser.add_argument('--covar', type=str, help='Read PLINK format covariate file contains covariates besides PCs to be adjusted')
+parser.set_defaults(covar="NULL")
+parser.add_argument('--pheno', type=str, help='Read PLINK format phenotype file [required]\nIf --mpheno is not specified then then 3rd column (the 1st phenotype) will be used.', required=True)
+parser.add_argument('--mpheno',type=int, default=1,help='Specify which phenotype to use from phenotype file (1 phenotype only)')
+parser.set_defaults(mpheno=1)
+parser.add_argument('--out',type=str, help='Specify the output file name. [required]',required=True)
+parser.add_argument('--std',action='store_true',default=False,help='Run SAdj-HE (i.e., with standardization)')
+
+args = parser.parse_args()
+#%%%
+prefix= "Example/grm"
+covar = "Example/covar.csv"
+pheno = "Example/pheno.phen"
+outprefix = "lkajfkjads"
+
+#%%
+outprefix = args.out
+
