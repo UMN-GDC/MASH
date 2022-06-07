bcf4f9e9243a8d83c6ede434068733fb543deab7
coffm049 <coffm049@umn.edu>
2022-06-07 10:19:56 -0500

    jkljaf

--- functions/types_n_valids.py
+++ functions/types_n_valids.py
@@ -8,15 +8,14 @@ Created on Tue Jun  7 08:25:47 2022
 
 #TODO: 
 #   - check for GRM files readable
-#   - check phenos, covars, pcs having 3+ columns
 
 
 import pandas as pd
 import argparse
 import os
 
-parser = argparse.ArgumentParser()
-covar = "/home/christian/Research/Stat_gen/tools/Basu_herit/Example/covar.txt"
+#parser = argparse.ArgumentParser()
+#covar = "/home/christian/Research/Stat_gen/tools/Basu_herit/Example/covar.txt"
 
 #%%
 
@@ -39,16 +38,18 @@ def three_col_file(path):
     """
     try :
         assert os.access(path, os.R_OK)
-        return os.path.abspath(path)
-        df = pd.read_table(path, sep = " ", header=0,  nrow=3) 
-        # if fewer than 3 columns raise an error
-        if df.shape[1] < 3 :
-            raise argparse.ArgumentTypeError(
-                "File at {} has fewer than three columns. Are you sure this file contians FID, IID, and then hte rest of the data?".format(path))
-        else :
-            return None
     except (AssertionError, OSError, TypeError) :
         raise argparse.ArgumentTypeError("Cannot read file at {}".format(path))
+        
+    # if fewer than 3 columns raise an error
+    try:
+        df = pd.read_table(path, sep = " ", header=0,  nrow=3) 
+        assert df.shape[1] > 2
+        return os.path.abspath(path)
+    except(AssertionError, TypeError) :
+        raise argparse.ArgumentTypeError(
+            "File at {} has fewer than three columns. Are you sure this file contians FID, IID, and then hte rest of the data?".format(path))
+
 #%%        
 
 
