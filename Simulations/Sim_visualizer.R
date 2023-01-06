a260de7c585eeef9303ce45ead689ee64e36eddf
coffm049 <coffm049@umn.edu>
2023-01-05 20:06:23 -0600

    Restructure simulation storage such that all simulation parameters are stored in a common folder and all results are stored in a common folder

--- Simulations/Sim_visualizer.R
+++ Simulations/Sim_visualizer.R
@@ -3,40 +3,49 @@ library(ggpattern)
 
 
 # Read data
-files <- dir("/home/christian/Research/Stat_gen/tools/Basu_herit/Simulations/Het/Results/5000/", 
+files <- dir(c("/home/christian/Research/Stat_gen/tools/Basu_herit/Simulations/Basic/Results/2000/",
+               "/home/christian/Research/Stat_gen/tools/Basu_herit/Simulations/EQUAL/Results/2000/",
+               "/home/christian/Research/Stat_gen/tools/Basu_herit/Simulations/IID/Results/2000/",
+               "/home/christian/Research/Stat_gen/tools/Basu_herit/Simulations/Het/Results/5000/"
+               ),
              recursive = T, pattern = "*.csv", full.names = T)
 
 df <- files %>%
   map(read_csv) %>%
-  reduce(rbind)
-
-
-# Annotate Estimators with family
-df <- df %>%
-  mutate(Estimator = str_replace(Estimator, "_est", ""),
-         Estimator = factor(Estimator, levels = c("GCTA", "nGCTA", "SWD", "Combat", "AdjHE_FE", "nAdjHE", "AdjHE_RE")),
+  reduce(rbind) %>%
+  # Annotate Estimators with family
+  mutate(Estimator = factor(Estimator, levels = c("GCTA", "nGCTA", "SWD", "Combat", "AdjHE", "nAdjHE", "AdjHE_RE")),
          Type = case_when(grepl("AdjHE", Estimator) ~ 'AdjHE',
                           grepl("GCTA", Estimator) ~ 'GCTA',
                           grepl("SWD", Estimator) ~ "Combat",
                           grepl("Combat", Estimator) ~ "Combat"
-         ))
+         ),
+         Estimate = ifelse(Type != "GCTA", Estimate *4/3, Estimate))
+
+
 # Dataframe for simulation sigmas
 sgs <- data.frame(sg = rep(c(1e-06, 0.25, 0.5), each = 2),
                   ss = rep(c(1e-06, 0.25), 3))
 
 
 # Visualization theme
+
+hlines<- data.frame(sg = rep(c(1e-06, 0.25, 0.5), 2),
+                    ss = rep(c(1e-06, 0.25), each = 3)) %>%
+  mutate(h2 = c(0, 1/3, 2/3, 0, 1/3, 2/3))
+
 Custom_vis <- function(df) {
   df %>%
     unite("Group", c(nsites, Estimator, sg, ss), sep = "_", remove = F) %>%
     ggplot(aes(x = Estimator, y = Estimate, fill = Type, group = Group, pattern= nsites)) +
     geom_boxplot_pattern() +
     theme(axis.text.x = element_text(angle = 90)) +
-    geom_hline(aes(yintercept = 0.5)) +
+    geom_hline(data = hlines, aes(yintercept = h2)) +
     xlab("") +
     ylab(expression(paste("Heritability Estimate", (hat(h^{2}))))) +
     guides(fill=guide_legend(title="Estimation Type"),
-           pattern = guide_legend(title = "# clusters"))
+           pattern = guide_legend(title = "# sites")) +
+    ylim(0, 1)
   
 }
 
@@ -47,25 +56,25 @@ apply(df, MARGIN =2 , function(x){length(unique(x))} )
 # Filter for simulation
 FILTER <- function(df) {
   df %>%
-    filter(nclusts == 5, site_het == "TRUE") 
+    # het filter
+    # filter(site_het == TRUE, nsubjects == 5000, nclusts == 5, nsites == 25)
+    # other filter
+    filter(site_het == TRUE, nsubjects == 2000, nclusts == 5, nsites == 25)
 }
 
-# Single panel
+
+
 df %>%
   FILTER() %>%
-  filter(sg == 0.5, ss == 0.25) %>%
-  Custom_vis()
-
-# Multipanel
-##############################################
-# Like this one for pulication
-
-
-df %>% 
-  FILTER() %>%
+  # Uncomment for Single panel
+  # filter(sg == 0.5, ss == 0.25) %>%
   Custom_vis() +
+  # Uncomment for multi panel
   facet_grid(rows = vars(ss), cols = vars(sg),
-             labeller = labeller(.rows = label_both, .cols = label_both))
+             labeller = labeller(.rows = label_both, .cols = label_both)) +
+  ggtitle("Site Heteroskedasticity")
+
+
 
 
 
---
+++ Simulations/joining_data.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 19:06:54 2023
+
+@author: christian
+"""
+
+from pathlib import Path
+import pandas as pd
+import numpy as np
+
+#%%
+
+path = '/home/christian/Research/Stat_gen/tools/Basu_herit/Simulations/'  # or unix / linux / mac path
+
+# Get the files from the path provided in the OP
+files = Path(path).glob('**/*.csv')  # .rglob to get subdirectories
+
+dfs = []
+for file in files:
+    if "Small_verifi" not in str(file):
+        df = pd.read_csv(file)
+        dfs.append(df)
+        
+df = pd.concat(dfs, ignore_index=True).to_csv("Full_results.csv",index= False, header= True, sep = ",")
