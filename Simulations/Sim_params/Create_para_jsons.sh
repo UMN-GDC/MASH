ee562f77e789bd0994529bd7b4058d7387e2c264
Christian Coffman <coffm049@ln0006.mesabi.msi.umn.edu>
2023-01-26 15:17:34 -0600

    Simulated heterogeneous and IID

--- Simulations/Sim_params/Create_para_jsons.sh
+++ Simulations/Sim_params/Create_para_jsons.sh
@@ -4,6 +4,6 @@ module load python3
 
 # Make json files for parallel simulations
 
-python ../../simulation_parallelizer.py --argfile two_5000.json 
+python ../../simulation_parallelizer.py --argfile two_5000_het_IID.json 
 
 
