9dbbb7fbe82434b1867171afc040beb5b36bb05d
coffm049 <coffm049@umn.edu>
2023-03-17 14:52:38 -0500

    Added ability to simulate from Plink files

--- AdjHE/simulation_helpers/Sim_generator.py
+++ AdjHE/simulation_helpers/Sim_generator.py
@@ -8,25 +8,40 @@ Created on Thu Oct 13 09:25:44 2022
 """
 import numpy as np
 import pandas as pd
+from pandas_plink import read_plink
 from AdjHE.simulation_helpers.sim_sites import sim_sites
 from AdjHE.simulation_helpers.sim_clusters import sim_pop_alleles, assign_clusters
 from AdjHE.simulation_helpers.sim_genos import sim_genos
 from AdjHE.simulation_helpers.sim_gene_effects import sim_gen_effects
 from AdjHE.simulation_helpers.sim_covariates import sim_covariates
 from AdjHE.simulation_helpers.sim_pheno import sim_pheno
+from AdjHE.simulation_helpers.sim_plink_pheno import sim_plink_pheno
 
 
 class pheno_simulator():
-    def __init__(self, nsubjects, nSNPs):
-        self.nsubjects = nsubjects
-        self.nSNPs = nSNPs
-
-        # Seed the simulated dataframe
-        self.df = pd.DataFrame({"FID": np.arange(start=1, stop=nsubjects + 1),
-                                "IID": np.arange(start=1, stop=nsubjects + 1),
-                                })
+    def __init__(self, nsubjects=1000, nSNPs=1000, plink_prefix=None):
         
-        self.rng = np.random.default_rng()
+        if plink_prefix == None :
+            self.nsubjects = nsubjects
+            self.nSNPs = nSNPs
+
+            # Seed the simulated dataframe
+            self.df = pd.DataFrame({"FID": np.arange(start=1, stop=nsubjects + 1),
+                                    "IID": np.arange(start=1, stop=nsubjects + 1)
+                                    })
+        
+            self.rng = np.random.default_rng()
+        else :
+            self.plink_prefix= plink_prefix
+            
+            (bim, fam, bed) = read_plink(plink_prefix)
+            (self.nSNPs, self.nsubjects) = bed.shape
+            self.df = pd.DataFrame({"FID": fam.fid,
+                                    "IID": fam.iid
+                                    })
+            # bed is (nSNPs x nsubjects) so transpose it
+            self.genotypes = bed.T
+            self.rng = np.random.default_rng()
 
 
     def sim_sites(self, nsites=1, eq_sites=False, random_BS = True):
@@ -99,19 +114,33 @@ class pheno_simulator():
                         nsites=30, theta_alleles=0.5, nclusts=5, dominance=5,
                         prop_causal=0.25, site_dep=False, nsubjects=1000,
                         nnpc=0, phens = 2, site_het = False, clusters_differ=False, cov_effect = True,
-                        ortho_cov = True, random_BS = True):
-        if site_comp == "EQUAL" :
-            eq_sites = True
-        else :
-            eq_sites= False
+                        ortho_cov = True, random_BS = True, npcs = 0):
         
-        # Run through full simulation and estimation
-        self.sim_sites(nsites= nsites, eq_sites=eq_sites, random_BS = random_BS)
-        self.sim_pops(theta_alleles=theta_alleles, nclusts=nclusts, site_comp= site_comp, dominance=dominance)
-        self.sim_genos(clusters_differ = clusters_differ, prop_causal=prop_causal)
-        self.sim_gen_effects(site_dep= site_dep)
-        self.sim_covars(cov_effect= cov_effect, ortho_cov = ortho_cov)
+        # Only simulate genes if plink file not specified
+        if self.plink_prefix == None: 
+  
+            if site_comp == "EQUAL" :
+                eq_sites = True
+            else :
+                eq_sites= False
+            
+            # Run through full simulation and estimation
+            self.sim_sites(nsites= nsites, eq_sites=eq_sites, random_BS = random_BS)
+            
+            self.sim_pops(theta_alleles=theta_alleles, nclusts=nclusts, site_comp= site_comp, dominance=dominance)
+            self.sim_genos(clusters_differ = clusters_differ, prop_causal=prop_causal)
+            self.sim_gen_effects(site_dep= site_dep)
+            self.sim_covars(cov_effect= cov_effect, ortho_cov = ortho_cov)
+            
+            for i in range(phens) :
+                self.sim_pheno(var_comps=sigma, phen = i, site_het = site_het)
         
-        for i in range(phens) :
-            self.sim_pheno(var_comps=sigma, phen = i, site_het = site_het)
+        else : 
+            for i in range(phens) :
+                if i ==0 :
+                    phenoname = "Y"
+                else :
+                    phenoname = "Y" + str(i)
+                self.df[phenoname] = sim_plink_pheno(rng = self.rng, bed = self.genotypes, sigma= sigma, prop_causal = prop_causal, npcs=npcs)
+                
 
