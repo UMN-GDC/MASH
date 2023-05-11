a4485cbe68a2d98626326f69599b5204e3590edf
coffm049 <coffm049@umn.edu>
2023-05-11 15:00:48 -0500

    End of Day

--- Simulator/simulation_helpers/run_sims.py
+++ Simulator/simulation_helpers/run_sims.py
@@ -21,18 +21,6 @@ from AdjHE.estimation.all_estimators import Basu_estimation
 
 rng = np.random.default_rng()
 
-#%%
-
-# nsubjects = 100; sigma = [0.5,0.25, 0.25]; site_comp = "IID"
-# nsites = 2; theta_alleles =0.5; nclusts =1
-# dominance=3; prop_causal=0.25; site_dep=False
-# nnpc = 1
-# nSNPs=20
-# rep = 2
-# phens = 2
-
-
-
 def sim_n_est(nsubjects = 1000, sigma = [0.5,0.25, 0.25], site_comp = "IID", nsites = 30,
               theta_alleles =0.5, nclusts =1, dominance=3, prop_causal=0.25, site_dep=False, nnpc = 1,
               nSNPs=20, phens = 2, site_het = False, clusters_differ = False, cov_effect = True,
@@ -40,7 +28,7 @@ def sim_n_est(nsubjects = 1000, sigma = [0.5,0.25, 0.25], site_comp = "IID", nsi
     sim = pheno_simulator(nsubjects= nsubjects, nSNPs = nSNPs)
     # Run through full simulation and estimation
     sim.full_sim(nsites= nsites, sigma= sigma, phens = phens, nclusts = nclusts, clusters_differ = clusters_differ,
-                 prop_causal = prop_causal, cov_effect = cov_effect, ortho_cov = ortho_cov, random_BS = True)
+                 prop_causal = prop_causal, cov_effect = cov_effect, ortho_cov = ortho_cov, random_BS = random_BS)
 
     ests = Basu_estimation()
     ests.df= sim.df
@@ -49,111 +37,56 @@ def sim_n_est(nsubjects = 1000, sigma = [0.5,0.25, 0.25], site_comp = "IID", nsi
     
     # Cretae covariates for sites if necessary    
     if cov_effect :
-        FE, RE= ["Xc"], ["Xc"]
+        FE= ["Xc"]
     else :
-        FE, RE = [], []
+        FE = []
         
     if nsites >1 :
         FE += ["abcd_site"]
     
-    # If fixed effect are still undefined, then make it empty
-    try :
-        FE
-    except NameError:
-        FE = []
-    
-    # If fixed effect are still undefined, then make it empty
-    try :
-        RE 
-    except NameError :
-        RE = []
-    
     logging.info(sim.df.columns.tolist())
-        
-        
-    # Estimate always
-    AdjHE = ests.estimate(Method = "AdjHE", npc = [nnpc], fixed_effects = FE, mpheno =ests.mpheno, Naive = False)
-    GCTA_est = ests.estimate(Method = "GCTA", npc = [nnpc], fixed_effects = FE, mpheno = ["Y"], Naive = False)
-    
-    # Estimate when greater than one site
-    if nsites > 1 :
-        nAdjHE = ests.estimate(Method = "AdjHE", npc = [nnpc], fixed_effects = RE, mpheno = ["Y"], random_groups = "abcd_site", Naive = True)
-        AdjHE_RE = ests.estimate(Method = "AdjHE", npc = [nnpc], fixed_effects = RE, mpheno = ["Y"], random_groups = "abcd_site", Naive = False)
+
+    # List of methods to estimate
+    Methods = ["AdjHE", "GCTA"]
+    # Meta 
+    Metas = [True, False]
+    # Treating pcs
+    pc_2moments = [True, False]
+    random_groups = [None, "abcd_site"]
+
+    full_results = pd.DataFrame({}, columns= ["Method", "Meta", "PC_moment", "RV", "h2", "V(h2)", "time"])
+    # iterate over all combinations of Methods, Meta, and pc_2moment 
+    for Method, Meta, pc_2nd, random_group in itertools.product(Methods, Metas, pc_2moments, random_groups):
         try :
-            nGCTA = ests.estimate(Method = "GCTA", npc = [nnpc], fixed_effects = RE, mpheno = ["Y"], random_groups = "abcd_site", Naive = True)
+            r = ests.estimate(Method = Method, npc = [nnpc], fixed_effects= FE, mpheno =ests.mpheno[0],
+                        Naive = Meta, pc_2moment = pc_2nd, random_groups = random_group)
+            result = pd.DataFrame({"Method" : Method,
+                                  "Meta" : Meta,
+                                  "PC_moment" : pc_2nd,
+                                  "RV" : random_group,
+                                  "h2" : r["h2"][0],
+                                  "Var(h2)" : r["var(h2)"][0],
+                                   "time" : r["time"][0]}, index= [0])
+            result["sg"] = sigma[0]
+            result["ss"] = sigma[1]
+            result["se"] = sigma[2]
+            result["nsubjects"] = nsubjects
+            result["nsites"] = nsites
+            result["theta_alleles"] = theta_alleles
+            result["nclusts"] = nclusts
+            result["prop_causal"] = prop_causal
+            result["nnpc"] = nnpc
+            result["nSNPs"] = nSNPs
+            result["site_comp"] = site_comp
+            result["dominance"] = dominance
+            result["site_dep"] = site_dep
+            result["site_het"] = site_het
+            
+            full_results = pd.concat([full_results, result], axis = 0)
         except FileNotFoundError :
-            logging.error("No estimate since sample size too small for GCTA")
-            nGCTA = np.nan
+            continue
 
-        SWD = ests.estimate(Method = "SWD", npc = [nnpc], fixed_effects = RE, mpheno = ["Y"], random_groups = "abcd_site", Naive = False)
-        Combat = ests.estimate(Method = "Combat", npc = [nnpc], fixed_effects = RE, mpheno = ["Y"], random_groups = "abcd_site", Naive = False)
-        Covbat = ests.estimate(Method = "Covbat", npc = [nnpc], fixed_effects = RE, mpheno = ["Y"], random_groups = "abcd_site", Naive = False)
-
-
-    else :
-        nAdjHE = pd.DataFrame({"h2" : np.nan, "var(h2)" : np.nan, "time" : np.nan}, index = [0])
-        AdjHE_RE = pd.DataFrame({"h2" : np.nan, "var(h2)" : np.nan, "time" : np.nan}, index = [0])
-        nGCTA = pd.DataFrame({"h2" : np.nan, "var(h2)" : np.nan, "time" : np.nan}, index = [0])
-        SWD = pd.DataFrame({"h2" : np.nan, "var(h2)" : np.nan, "time" : np.nan}, index = [0])
-        Combat = pd.DataFrame({"h2" : np.nan, "var(h2)" : np.nan, "time" : np.nan}, index = [0])
-        Covbat = pd.DataFrame({"h2" : np.nan, "var(h2)" : np.nan, "time" : np.nan}, index = [0])
-
-    result = {"GCTA" : GCTA_est["h2"][0],
-              "var_GCTA" : GCTA_est["var(h2)"][0],
-              "time_GCTA" : GCTA_est["time"][0],
-              
-              "nGCTA": nGCTA["h2"][0],
-              "var_nGCTA" : nGCTA["var(h2)"][0],
-              "time_nGCTA" : nGCTA["time"][0],
-
-              "nAdjHE": nAdjHE["h2"][0],
-              "var_nAdjHE" : nAdjHE["var(h2)"][0],
-              "time_nAdjHE" : nAdjHE["time"][0],
-
-              "AdjHE": AdjHE["h2"][0],
-              "var_AdjHE" : AdjHE["var(h2)"][0],
-              "time_AdjHE" : AdjHE["time"][0],
-
-              "SWD": SWD["h2"][0],
-              "var_SWD" : SWD["var(h2)"][0],
-              "time_SWD" : SWD["time"][0],
-              
-              "Combat": Combat["h2"][0],
-              "var_Combat" : Combat["var(h2)"][0],
-              "time_Combat" : Combat["time"][0],
-              
-              "Covbat": Combat["h2"][0],
-              "var_Covbat" : Combat["var(h2)"][0],
-              "time_Covbat" : Combat["time"][0],
-
-              "AdjHE_RE" : AdjHE_RE["h2"][0],
-              "var_AdjHE_RE" : AdjHE_RE["var(h2)"][0],
-              "time_AdjHE_RE" : AdjHE_RE["time"][0],
-              #["MOM_est",MOM_est]
-              }
         
-    # store simulation details
-    result["sg"] = sigma[0]
-    result["ss"] = sigma[1]
-    result["se"] = sigma[2]
-    result["nsubjects"] = nsubjects
-    result["nsites"] = nsites
-    result["theta_alleles"] = theta_alleles
-    result["nclusts"] = nclusts
-    result["prop_causal"] = prop_causal
-    result["nnpc"] = nnpc
-    result["nSNPs"] = nSNPs
-    result["site_comp"] = site_comp
-    result["dominance"] = dominance
-    result["site_dep"] = site_dep
-    result["site_het"] = site_het
-    
-    result= pd.DataFrame(result, index = [0])        
-
-    return result
-
-#%%
-
 def sim_experiment(nsubjectss = [1000], sigmas = [[0.5,0.25, 0.25]], site_comps = ["IID"], nsites = [25],
               theta_alleless = [0.9], nclustss = [5], dominances= [3], prop_causals= [0.05], site_deps= [False], nnpcs = [1],
               nSNPss= [200], phenss= [2], reps = 10, site_het = False, clusters_differ = False, cov_effect = True,
@@ -200,15 +133,15 @@ def sim_experiment(nsubjectss = [1000], sigmas = [[0.5,0.25, 0.25]], site_comps 
 #         elif (sg ==0) and (ss == 0) :
 #             sigmas += [[sg, ss, se]]
 #%%  
-N  = 500
-ns = 5
-nc = 2
-sigmas = [[0.5,0.25,0.25]]
+#N  = 500
+#ns = 5
+#nc = 2
+#sigmas = [[0.5,0.25,0.25]]
 #%%
 
-df = sim_experiment(nsubjectss= [N], reps= 5, nsites=[1], site_comps = ["EQUAL"], sigmas = sigmas, nnpcs = [1], nclustss=[nc], 
-                    ortho_cov = True, cov_effect= True, phenss= [3], random_BS = False)
-logging.info(df[["GCTA", "AdjHE", "AdjHE_RE", "Combat", "SWD"]].mean())
+#df = sim_experiment(nsubjectss= [N], reps= 5, nsites=[1], site_comps = ["EQUAL"], sigmas = sigmas, nnpcs = [1], nclustss=[nc], 
+#                    ortho_cov = True, cov_effect= True, phenss= [3], random_BS = False)
+#logging.info(df[["GCTA", "AdjHE", "AdjHE_RE", "Combat", "SWD"]].mean())
 
 #%%
 # # df.to_csv("Simulations/Sim_working_Combat1.csv", header=  True, index= False)
