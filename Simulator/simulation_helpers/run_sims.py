f327d062f02431bed5c9fdbf272faddceda1527e
coffm049 <coffm049@umn.edu>
2023-05-25 12:01:54 -0500

    Fixed small bug where I wasn't returning the results from the sim_n_estimate function to the encapsulating sim_experiment looping function

--- Simulator/simulation_helpers/run_sims.py
+++ Simulator/simulation_helpers/run_sims.py
@@ -49,7 +49,6 @@ def sim_n_est(nsubjects = 1000, sigma = [0.5,0.25, 0.25], site_comp = "IID", nsi
     full_results = pd.DataFrame({}, columns= ["Method", "Meta", "PC_moment", "RV", "h2", "V(h2)", "time"])
     # iterate over all combinations of Methods, Meta, and pc_2moment 
     for (Met, M, pc2, rg) in itertools.product(["AdjHE", "GCTA"], [True, False], [True, False], [None, "abcd_site"]):
-        print(Met, M,pc2,rg)
         
         try :
             r = ests.estimate(Method = Met, npc = [nnpc], fixed_effects= FE, mpheno =ests.mpheno[0],
@@ -75,11 +74,13 @@ def sim_n_est(nsubjects = 1000, sigma = [0.5,0.25, 0.25], site_comp = "IID", nsi
                                    "dominance" : dominance,
                                    "site_dep" : site_dep,
                                    "site_het" : site_het}, index= [0])
-            full_results = pd.concat([full_results, result], axis = 0)
+            full_results = pd.concat([full_results, result], ignore_index= True)
         except TypeError :
             pass
         except np.linalg.LinAlgError :
             pass
+
+        return full_results
         
 def sim_experiment(nsubjectss = [1000], sigmas = [[0.5,0.25, 0.25]], site_comps = ["IID"], nsites = [25],
               theta_alleless = [0.9], nclustss = [5], dominances= [3], prop_causals= [0.05], site_deps= [False], nnpcs = [1],
@@ -104,7 +105,8 @@ def sim_experiment(nsubjectss = [1000], sigmas = [[0.5,0.25, 0.25]], site_comps 
                            site_dep= site_dep, nnpc = nnpc,
                            nSNPs=nSNPs, phens = phens, site_het = site_het, clusters_differ = clusters_differ, cov_effect = cov_effect,
                            ortho_cov = ortho_cov, random_BS = random_BS)
-        sim_results= sim_results.append(result, ignore_index = True)
+        print(result)
+        sim_results = pd.concat([sim_results, result], ignore_index=True)
         # Remove any temps
     return sim_results
                                                      
