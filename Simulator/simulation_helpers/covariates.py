d21a05d563992065549281d3bf278939455b2673
coffm049 <coffm049@umn.edu>
2023-06-23 14:34:04 -0500

    Removed options for including covariate effects and orthogonal covariate effects. This seemed like needless complication.

--- Simulator/simulation_helpers/covariates.py
+++ Simulator/simulation_helpers/covariates.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 import scipy
 
-def sim_covariates(rng, nclusts, df, cov_effect=True, ortho_cov = False): 
+def sim_covariates(rng, nclusts, df): 
     """
     Generate the covariates as well as associated effects for each subject
 
@@ -21,11 +21,6 @@ def sim_covariates(rng, nclusts, df, cov_effect=True, ortho_cov = False):
         number of genetic clusters in the dataset.
     df : pandas datafraem
         dataframe containing information on each subject for the simulation.
-    cov_effect : bool, optional
-        Should the covariates cause a fixed effect or no effect at all?. The default is True.
-    ortho_cov : bool, optional
-        should the covariates be orthogonal to principal components. This is generally overkill for high dimensional data where
-        n>> p. The default is False.
 
     Returns
     -------
@@ -38,22 +33,10 @@ def sim_covariates(rng, nclusts, df, cov_effect=True, ortho_cov = False):
     # To generate orthogonal, find the Null space of the transpose of the principal components and site dummy matrix
     # Then, construct a random vector from the null space.
     nsubjects = df.shape[0]
-    if ortho_cov :
-        pcs = ["pc_" + str(i + 1) for i in range(nclusts)]
-        cov_arr= pd.get_dummies(df["abcd_site"].astype(str)).to_numpy()
-        cov_arr = np.concatenate((cov_arr, df[pcs].to_numpy()), axis =1 )
-        ns = scipy.linalg.null_space(cov_arr.T)
-        Xc = np.dot(ns, np.random.rand(ns.shape[1],1))
 
-    else: 
-        Xc = rng.uniform(0,1, nsubjects)
+    Xc = rng.uniform(0,1, nsubjects)
     
-    
-    if cov_effect :
-        # Scale it to be on same size as genetic contribution
-        Covar_contrib = Xc * np.mean(df["Gene_contrib"])
-    else :
-        df["Covar_contrib"] = 0 
+    Covar_contrib = Xc * np.mean(df["Gene_contrib"])
         
     return Xc, Covar_contrib
 
