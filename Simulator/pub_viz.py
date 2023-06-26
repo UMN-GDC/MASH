cdcb0fa0692e7e8d665d4ec9b494042f42402217
coffm049 <coffm049@umn.edu>
2023-06-26 15:16:24 -0500

    Redid simulated SNP effects figures

--- Simulator/pub_viz.py
+++ Simulator/pub_viz.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from Simulator.simulation_helpers.Sim_generator import pheno_simulator
 
 #%% vizualize different alphas on SNP effect size
 x = np.linspace(0, 0.25, 20)
@@ -9,7 +9,18 @@ for i in [0,0.25, 0.5, 0.75,1] :
     plt.plot(x, (x * (1-x))**(-i), label = "alpha= " + str(i)),
 plt.legend()
 plt.xlabel("MAF")
-plt.ylabel("SNP effect size")
-plt.savefig(fname = "docs/Figures/Sim_features/SNPalphaControl.png", dpi = 300, format = "png")
+plt.ylabel("Variance of SNP effect")
+plt.savefig(fname = "docs/Papers/Toolbox_paper/figures/SNPalphaControl.png", dpi = 300, format = "png")
+plt.clf()
 
+#%% vizualize example simulated effects
+
+sim = pheno_simulator(nsubjects= 1000, nSNPs = 1000)
+sim.full_sim()
+
+MAF = 0.5 - abs(0.5 - sim.genotypes[:, sim.causals].mean(axis= 0)/2)
+plt.scatter(x = MAF, y=  abs(sim.SNP_effects))
+plt.xlabel("MAF")
+plt.ylabel("SNP effect")
+plt.savefig(fname = "docs/Papers/Toolbox_paper/figures/simmedSNPeffects.png", dpi = 300, format = "png")
 
