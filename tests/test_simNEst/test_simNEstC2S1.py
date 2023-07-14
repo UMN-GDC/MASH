407422e8a12f9a35c5de820b72255b6281012f12
coffm049 <coffm049@umn.edu>
2023-07-14 15:07:49 -0500

    Completed building tests for 1 and 2 clusters for a single site

--- tests/test_simNEst/test_simNEstC2S1.py
+++ tests/test_simNEst/test_simNEstC2S1.py
@@ -4,31 +4,47 @@ from Simulator.simulation_helpers.Sim_generator import pheno_simulator
 import numpy as np
 import pytest
 
-#%% Testing for Multiple sites and single cluster
+#%% Basic testing for single site and cluster
+
 @pytest.fixture
-def C1S25() :
-    rng = np.random.default_rng(12)
-    sim = pheno_simulator(rng = rng, nsubjects= 1000)
+def C2S1() :
+    rng = np.random.default_rng(123)
+    sim = pheno_simulator(rng = rng, nsubjects= 500)
     sim.sim_sites(nsites =1)
     sim.sim_pops(nclusts= 2)
     sim.sim_genos()
-    sim.sim_gen_effects()
-    sim.sim_covars()
-    sim.sim_pheno(h2 = 0.5)
+    sim.sim_pheno(h2Hom = 0.4, h2Het= [0.1, 0.1], alpha = -1)
     est = Basu_estimation()
     est.GRM = sim.GRM
     est.df = sim.df
-    return est
+    return est 
 
-@pytest.mark.C1S25
-def test_simNGCTA(C1S25) :
-    est = C1S25
+@pytest.mark.C2S1
+def test_simNGCTA(C2S1) :
+    est = C2S1
     result = est.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "GCTA", fixed_effects= ["Xc"])
-    assert result["h2"][0] == pytest.approx(0.5, abs = 0.05)
+    assert result["h2"][0] == pytest.approx(0.5, abs = 0.05) 
 
-@pytest.mark.C1S25
-def test_simNAdjHE(C1S25) :
-    est = C1S25
+@pytest.mark.C2S1
+def test_simAdjHE(C2S1):
+    est = C2S1
     result = est.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "AdjHE", fixed_effects= ["Xc"])
-    assert result["h2"][0] == pytest.approx(0.5, abs = 0.05)
+    assert result["h2"][0] == pytest.approx(0.5, abs = 0.05) 
 
+#%%
+@pytest.mark.C1S1_thorough
+def test_thoroughC1S1(): 
+    rng = np.random.default_rng(12)
+    h2s = np.zeros(25)
+    for i in range(25) : 
+        sim = pheno_simulator(rng = rng, nsubjects= 250)
+        sim.sim_sites(nsites =1)
+        sim.sim_pops(nclusts= 1)
+        sim.sim_genos()
+        sim.sim_gen_effects()
+        sim.sim_covars()
+        sim.sim_pheno(h2 = 0.5)
+        est = Basu_estimation()
+        est.GRM = sim.GRM
+        est.df = sim.df
+        h2s[i]= est.estimate(mpheno = ["Y0"], npc = [0], Method = "GCTA", fixed_effects= ["Xc"])["h2"][0]
