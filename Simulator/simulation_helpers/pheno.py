ddf58574427e2ef758d074f54a7e5d3b517da5cb
coffm049 <coffm049@umn.edu>
2023-07-14 15:08:09 -0500

    Changed simulation of phenotype and verified that we get heritabilities close to desired

--- Simulator/simulation_helpers/pheno.py
+++ Simulator/simulation_helpers/pheno.py
@@ -53,29 +53,29 @@ def sim_pheno(rng, genotypes, df, h2Hom, h2Het, alpha = -1, phenoname = "Y0"):
 
     # sample shared (homogeneous) effects from normal with variance proportional to some function of global allele frequency
     prop = (freqs * (1-freqs))**alpha
-    homo_eff = rng.normal(np.repeat(0, Xcausal.shape[1]), prop, size =  Xcausal.shape[1])
+    homo_eff = rng.normal(np.repeat(0, nCausal), prop, size =  nCausal)
     # make sure no infinities
     homo_eff[np.isinf(homo_eff)] = 0
     homo_contrib = np.array(np.dot(Xcausal, homo_eff)).flatten()
     df["homo_contrib"] = homo_contrib * np.sqrt(h2Hom / np.var(homo_contrib))
     nclusts = df.subj_ancestries.nunique()
-    cluster_eff = np.zeros((nSNPs, nclusts))
+    cluster_eff = np.zeros((nCausal, nclusts))
     cluster_contrib = np.zeros(nsubjects)
     errors = np.zeros(nsubjects)
     # cluster specific effects
     if nclusts > 1 :
         for cluster in range(nclusts) :
             cluster_position = df.subj_ancestries == cluster 
-            cluster_freq = np.asarray(np.mean(genotypes[cluster_position, :], axis = 0)/2).flatten()
+            cluster_freq = np.asarray(np.mean(genotypes[cluster_position,:][:,causals], axis = 0)/2).flatten()
             prop = (cluster_freq * (1- cluster_freq)) ** alpha
-            cluster_eff[:,cluster] = rng.normal(np.repeat(0, Xcausal.shape[1]), prop, size =  Xcausal.shape[1])
+            cluster_eff[:,cluster] = rng.normal(np.repeat(0, nCausal), prop, size =  nCausal)
             cluster_eff[np.isinf(cluster_eff)] = 0
-
-            temp_contrib = np.array(np.dot(Xcausal[cluster_position, :], cluster_eff)).flatten()
+            cluster_contrib[cluster_position] = np.array(np.dot(Xcausal[cluster_position, :], cluster_eff[:,cluster])).flatten()
+            print(cluster_contrib.shape)
             # rescale
-            cluster_contrib[cluster_position]= temp_contrib * np.sqrt(h2Het[cluster]/ np.var(temp_contrib))
+            cluster_contrib[cluster_position]= cluster_contrib[cluster_position] * np.sqrt(h2Het[cluster]/ np.var(cluster_contrib[cluster_position]))
 
-            cluster_error = rng.normal(0, 1, cluster_contrib.sum())
+            cluster_error = rng.normal(0, 1, cluster_position.sum())
             errors[cluster_position] = cluster_error * np.sqrt((1- h2Hom - h2Het[cluster]) / np.var(cluster_error))
     else :
         errors = rng.normal(0, np.sqrt(1-h2Hom), nsubjects)
@@ -85,8 +85,8 @@ def sim_pheno(rng, genotypes, df, h2Hom, h2Het, alpha = -1, phenoname = "Y0"):
     df["errors"] = errors
     df["Xc"] = rng.uniform(0, 1, nsubjects)
     Beta_c = 0.5
-    df["Covar_contrib"] = Beta_c * df["Xc"] 
+    df["Covar_contrib"] = Beta_c * df["Xc"]
+    df[["Covar_contrib", "homo_contrib", "cluster_contrib", "errors"]] =df[["Covar_contrib", "homo_contrib", "cluster_contrib", "errors"]]- df[["Covar_contrib", "homo_contrib", "cluster_contrib", "errors"]].mean()  
     df[str(phenoname)] = df[["Covar_contrib", "homo_contrib", "cluster_contrib", "errors"]].sum(axis = 1)
-    df[str(phenoname)] = df[str(phenoname)] - np.mean(df[str(phenoname)])   
 
     return df, causals, homo_eff, cluster_eff 
