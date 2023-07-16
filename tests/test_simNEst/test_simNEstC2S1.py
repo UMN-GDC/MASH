673f6ab600f24ea429d7863efacb2b3625927e63
coffm049 <coffm049@umn.edu>
2023-07-16 06:41:18 -0500

    Made a test for each separate genetic cluster, and it looks like we are properly controlling for heritability within each.

--- tests/test_simNEst/test_simNEstC2S1.py
+++ tests/test_simNEst/test_simNEstC2S1.py
@@ -9,11 +9,11 @@ import pytest
 @pytest.fixture
 def C2S1() :
     rng = np.random.default_rng(123)
-    sim = pheno_simulator(rng = rng, nsubjects= 500)
+    sim = pheno_simulator(rng = rng, nsubjects= 1000)
     sim.sim_sites(nsites =1)
     sim.sim_pops(nclusts= 2)
     sim.sim_genos()
-    sim.sim_pheno(h2Hom = 0.4, h2Het= [0.1, 0.1], alpha = -1)
+    sim.sim_pheno(h2Hom = 0.2, h2Het= [0.2, 0.6], alpha = 0)
     est = Basu_estimation()
     est.GRM = sim.GRM
     est.df = sim.df
@@ -32,19 +32,21 @@ def test_simAdjHE(C2S1):
     assert result["h2"][0] == pytest.approx(0.5, abs = 0.05) 
 
 #%%
-@pytest.mark.C1S1_thorough
-def test_thoroughC1S1(): 
-    rng = np.random.default_rng(12)
-    h2s = np.zeros(25)
-    for i in range(25) : 
-        sim = pheno_simulator(rng = rng, nsubjects= 250)
-        sim.sim_sites(nsites =1)
-        sim.sim_pops(nclusts= 1)
-        sim.sim_genos()
-        sim.sim_gen_effects()
-        sim.sim_covars()
-        sim.sim_pheno(h2 = 0.5)
-        est = Basu_estimation()
-        est.GRM = sim.GRM
-        est.df = sim.df
-        h2s[i]= est.estimate(mpheno = ["Y0"], npc = [0], Method = "GCTA", fixed_effects= ["Xc"])["h2"][0]
+@pytest.mark.C2S1_thorough
+def test_thoroughC1S1(C2S1): 
+    est= C2S1
+    df1= est.df.query("subj_ancestries==0")
+    G1 = est.GRM[df1.index, :][:, df1.index]
+    df2 = est.df.query("subj_ancestries==1")
+    G2 = est.GRM[df2.index, :][:, df2.index]
+    
+    est1 = Basu_estimation()
+    est1.GRM = G1
+    est1.df = df1
+    
+    est2 = Basu_estimation()
+    est2.GRM = G2
+    est2.df = df2
+
+    result1 = est1.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "GCTA", fixed_effects= ["Xc"])
+    result2 = est2.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "GCTA", fixed_effects= ["Xc"])
