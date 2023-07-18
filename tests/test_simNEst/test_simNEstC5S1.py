c1950b0c2c2a88bc301b7cd4358dcdf0267a6012
coffm049 <coffm049@umn.edu>
2023-07-18 17:58:41 -0500

    tested AdjHE for multiple clusters and they are looking somewhat questionable

--- tests/test_simNEst/test_simNEstC5S1.py
+++ tests/test_simNEst/test_simNEstC5S1.py
@@ -13,7 +13,7 @@ def C5S1() :
     sim.sim_sites(nsites =1)
     sim.sim_pops(nclusts= 5)
     sim.sim_genos()
-    sim.sim_pheno(h2Hom = 0.2, h2Het= [0.2, 0.4, 0.6, 0.8, 0.0], alpha = -1)
+    sim.sim_pheno(h2Hom = 0.6, h2Het= [0, 0, 0, 0, 0], alpha = -1)
     est = Basu_estimation()
     est.GRM = sim.GRM
     est.df = sim.df
@@ -28,7 +28,13 @@ def test_simNGCTA(C5S1) :
 @pytest.mark.C5S1
 def test_simAdjHE(C5S1):
     est = C5S1
-    result = est.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "AdjHE", fixed_effects= ["Xc"])
+    result = est.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "AdjHE", fixed_effects= ["Xc"], PC_effect = "fixed")
+    print(result)
+    result = est.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "AdjHE", fixed_effects= ["Xc"], PC_effect = "mixed")
+    print(result)
+    result = est.estimate(mpheno = ["Y0"], npc = [0,1,2], Method = "AdjHE", fixed_effects= ["Xc"], PC_effect = "random")
+    print(result)
+
     assert result["h2"][0] == pytest.approx(0.5, abs = 0.05) 
 
 #%%
