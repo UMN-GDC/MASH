1a3d3ea3cbef06a1ebba866b065272c419596d96
coffm049 <coffm049@umn.edu>
2023-07-20 14:00:36 -0500

    Removed a prototype file

--- testing_variance.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-# Add the additional alpha +1 so that it can simulate for both alpha = 0 and alapha = -1
-homo_eff = rng.normal(np.repeat(0, nSNPs), np.sqrt(sig_g / nSNPs * (2 * prop) ** alpha  * 2 ** (alpha+ 1)), size =  nSNPs)
-
