9c020ba4c4492970c9c6c1394c099b7584059cf0
coffm049 <coffm049@umn.edu>
2023-09-01 11:15:14 -0500

    Added ability to  simulate with plink style input

--- docs/Tutorials/SimPlinkPheno.py
+++ docs/Tutorials/SimPlinkPheno.py
@@ -5,9 +5,12 @@ from Simulate.simulation_helpers.Sim_generator import pheno_simulator
 import numpy as np
 import pytest
 
+
+# The dataframe needs to have the subject ancestries contained in a column called "subj_ancestries"
+
 rng = np.random.default_rng(123)
 # Instantiate a simulator
-sim = pheno_simulator(rng = rng, plink_prefix="Estimate/examples/Genotype_files/geno")
+sim = pheno_simulator(rng = rng, plink_prefix="Estimate/examples/Genotype_files/geno", ancestries = None)
 # Detemrine how many sites, genetic clusters you want
 sim.full_sim(nclusts=2, nsites=2)
 
