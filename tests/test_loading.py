f0c39821b9fd3c86bb4038955f3abc42716577f1
coffm049 <coffm049@umn.edu>
2026-05-07 13:38:59 -0500

    Add iid_col and fid_col to test configs and tests

--- tests/test_loading.py
+++ tests/test_loading.py
@@ -54,6 +54,60 @@ def test_loading_all() :
     'pc_1', 'pc_2', 'pc_3', 'pc_4', 'pc_5', 'pc_6', 'pc_7', 'pc_8', 'pc_9', 'pc_10', 'Tidyness', 
     'Happy_camper', 'Like_of_levis', 'abcd_site'] == ests.df.columns.tolist()
 
+
+@pytest.mark.loading
+def test_argfile_iid_fid_cols():
+    """Test that iid_col and fid_col are correctly read from argfile."""
+    args = read_flags({"argfile": "tests/test_data/config_AdjHE.json"})
+    assert args.get("iid_col") == "IID"
+    assert args.get("fid_col") == "FID"
+
+
+@pytest.mark.loading
+def test_argfile_default_iid_fid():
+    """Test default iid_col and fid_col when not specified in argfile."""
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--iid-col', type=str, default='IID')
+    parser.add_argument('--fid-col', type=str, default='FID')
+    args = parser.parse_args([])
+    assert args.iid_col == 'IID'
+    assert args.fid_col == 'FID'
+
+
+@pytest.mark.loading
+def test_argfile_custom_iid_fid_cols():
+    """Test that custom iid_col and fid_col can be specified in argfile."""
+    custom_config = {
+        "PC": "tests/test_data/EUR2.2.eigenvec",
+        "covar": ["tests/test_data/EUR_covariates.cov"],
+        "prefix": "tests/test_data/EUR2",
+        "pheno": "tests/test_data/EUR_simulation2.pheno",
+        "out": "/users/4/coffm049/software/MASH/tests/results/EUR_custom_cols",
+        "npc": [2],
+        "mpheno": [1],
+        "k": 0,
+        "std": False,
+        "argfile": None,
+        "loop_covars": False,
+        "covar_relates": False,
+        "random_groups": None,
+        "Naive": False,
+        "Method": "AdjHE",
+        "ids": None,
+        "qcovar": None,
+        "covar_discrete": None,
+        "iid_col": "SubjectID",
+        "fid_col": "FamilyID"
+    }
+    import json
+    with open("/users/4/coffm049/software/MASH/tests/test_data/config_custom_ids.json", "w") as f:
+        json.dump(custom_config, f)
+    
+    args = read_flags({"argfile": "/users/4/coffm049/software/MASH/tests/test_data/config_custom_ids.json"})
+    assert args.get("iid_col") == "SubjectID"
+    assert args.get("fid_col") == "FamilyID"
+
     
 
         
